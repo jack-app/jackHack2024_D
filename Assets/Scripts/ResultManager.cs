a858e52d0d3ea13aa05cbf7485d0b099da48e523
yokoshi1123 <yokota.koshi.g6@s.mail.nagoya-u.ac.jp>
2024-05-05 15:23:40 +0900

    ジョージと吹き出しを追加

--- Assets/Scripts/ResultManager.cs
+++ Assets/Scripts/ResultManager.cs
@@ -6,18 +6,40 @@ using TMPro;
 public class ResultManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreText;
-    
+    [SerializeField] private TextMeshProUGUI commentText;
+
     [SerializeField] private int correctNumber = 0;
+
+    string[] comments =  { "<size=48>厳しいって。\nモテないって。", "<size=48>男としてのパーツが\n足りてないって。", "<size=48>モテないよ、普通に。", "<size=36>女の子はどういう男性を\n求めてる？格上の存在だよね。\r\n", "<size=36>全問正解できないようじゃ\n弱いって。", "<size=48>これくらいできて\n当然だって。" };
     
     // Start is called before the first frame update
     void Start()
     {
+        //collectNumber = ;
         scoreText.text = "Score: " + correctNumber.ToString();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (correctNumber < 5)
+        {
+            commentText.text = comments[0];
+        }
+        else if (correctNumber < 10)
+        {
+            commentText.text = comments[1];
+        }
+        else if (correctNumber < 15)
+        {
+            commentText.text = comments[2];
+        }
+        else if (correctNumber < 20)
+        {
+            commentText.text = comments[3];
+        }
+        else if (correctNumber < 25) 
+        {
+            commentText.text = comments[4];
+        }
+        else if (correctNumber <= 30)
+        {
+            commentText.text = comments[5];
+        }
     }
 }
